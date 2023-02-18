7e3441da0db1392959df13a15d679e95576489f9
Jeff Leverenz <jeff.leverenz@gmail.com>
2023-02-17 22:26:02 -0600

    Add argument for root dir

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 )
 
@@ -14,9 +15,14 @@ type SizeInfo struct {
 func main() {
 	fmt.Println("sizeup")
 
+	root := "."
+	if len(os.Args) > 1 {
+		root = os.Args[1]
+	}
+
 	infos := []SizeInfo{}
 
-	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
